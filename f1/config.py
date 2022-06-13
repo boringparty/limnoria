38271e024d11837c77f5889afbf3c85c5159a358
Hello Party <1559567+hello-party@users.noreply.github.com>
2022-06-12 20:33:45 -0700

    Create config.py

---
+++ f1/config.py
@@ -0,0 +1,24 @@
+import supybot.conf as conf
+import supybot.registry as registry
+
+try:
+    from supybot.i18n import PluginInternationalization
+
+    _ = PluginInternationalization("F1")
+except:
+    # Placeholder that allows to run the plugin on a bot
+    # without the i18n module
+    _ = lambda x: x
+
+
+def configure(advanced):
+    # This will be called by supybot to configure this module.  advanced is
+    # a bool that specifies whether the user identified themself as an advanced
+    # user or not.  You should effect your configuration by manipulating the
+    # registry as appropriate.
+    from supybot.questions import expect, anything, something, yn
+
+    conf.registerPlugin("F1", True)
+
+
+Advice = conf.registerPlugin("F1")
