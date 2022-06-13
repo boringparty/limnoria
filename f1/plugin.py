9051668adff303c299fec934ed6be78a41a4e7b4
Hello Party <1559567+hello-party@users.noreply.github.com>
2022-06-13 01:18:03 -0700

    Update plugin.py

--- f1/plugin.py
+++ f1/plugin.py
@@ -1,3 +1,7 @@
+# this accepts @champ or @constructor with an optional year
+# and also @gp with an optional race number for the current season
+
+
 import supybot.utils as utils
 from supybot.commands import *
 import supybot.plugins as plugins
@@ -22,7 +26,7 @@ class F1(callbacks.Plugin):
 
     threaded = True
 
-    def f1(self, irc, msg, args, year):
+    def champ(self, irc, msg, args, year):
         """<year>
         Call standings by year
         F1 Standings
@@ -30,23 +34,68 @@ class F1(callbacks.Plugin):
 
         data = requests.get("https://ergast.com/api/f1/current/driverStandings.json")
         if year:
-            data = requests.get(
-                "https://ergast.com/api/f1/%s/driverStandings.json" % (year)
-            )
-        driver_standings = json.loads(data.content)["MRData"]["StandingsTable"][
-            "StandingsLists"
-        ][0]["DriverStandings"]
+            data = requests.get("https://ergast.com/api/f1/%s/driverStandings.json" % (year))
+        driver_standings = json.loads(data.content)["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"]
         string_segments = []
+
+
 
         for driver in driver_standings:
             name = driver["Driver"]["code"]
             position = driver["positionText"]
             points = driver["points"]
-            string_segments.append(f"\x031,82 {name} {points} \x0F")
+            string_segments.append(f'\x035{name}\x0F {points}')
 
-        irc.reply(" ".join(string_segments))
+        irc.reply(", ".join(string_segments))
 
-    f1 = wrap(f1, [optional("int")])
+    champ = wrap(champ,[optional("int")])
+
+
+    def gp(self, irc, msg, args, race):
+        """<year>
+        Call standings by year
+        F1 Standings
+        """
+
+        data = requests.get("https://ergast.com/api/f1/current/last/results.json")
+        if race:
+            data = requests.get("https://ergast.com/api/f1/current/%s/results.json" % (race))
+        driver_result = json.loads(data.content)["MRData"]["RaceTable"]["Races"][0]["Results"]
+        string_segments = []
+
+        for driver in driver_result:
+            name = driver["Driver"]["code"]
+            position = driver["positionText"]
+            points = driver["points"]
+            string_segments.append(f'{position} \x035{name}\x0F {points}')
+
+        irc.reply(", ".join(string_segments))
+
+    gp = wrap(gp,[optional("int")])
+
+
+    def constructor(self, irc, msg, args, year):
+        """<year>
+        Call standings by year
+        F1 Standings
+        """
+
+        data = requests.get("https://ergast.com/api/f1/current/constructorStandings.json")
+        if year:
+            data = requests.get("https://ergast.com/api/f1/current/constructorStandings.json" % (year))
+        driver_result = json.loads(data.content)["MRData"]["StandingsTable"]["StandingsLists"][0]["ConstructorStandings"]
+        string_segments = []
+
+        for driver in driver_result:
+            name = driver["Constructor"]["name"]
+            position = driver["positionText"]
+            points = driver["points"]
+            string_segments.append(f'{position} \x035{name}\x0F {points}')
+
+        irc.reply(", ".join(string_segments))
+
+    constructor  = wrap(constructor,[optional("int")])
+
 
 
 Class = F1
